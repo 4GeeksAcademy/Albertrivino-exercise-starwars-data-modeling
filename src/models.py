bf701aa319d60f005542f6485f1e186bb64eaba2
Albertrivino <155556342+Albertrivino@users.noreply.github.com>
2024-08-19 10:32:10 +0000

    *Albertrivino [Project StarWars Data Modeling]

--- src/models.py
+++ src/models.py
@@ -7,23 +7,58 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    favoritesId  = Column(Integer, ForeignKey('favorites.id'))
+    userName = Column(String(25), unique=True)
+    firstName = Column(String(30),nullable=False)
+    lastName = Column(String(30),nullable=False)
+    email = Column(String(40),unique=True, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    charactersId = Column (Integer, ForeignKey ('characters.id'))
+    planetsId = Column (Integer, ForeignKey ('planets.id'))
+    starshipsId = Column(Integer, ForeignKey ('starships.id'))
+    vehiclesId = Column(Integer, ForeignKey ('vehicles.id'))
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25))
+    birthYear = Column(String(25))
+    species = Column(String(25))
+    height = Column(String(25))
+    mass = Column(String(25))
+
+class Planets (Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25))
+    population = Column(String(25))
+    orbitalPeriod = Column(String(25))
+    diameter = Column(String(25))
+    gravity = Column(String(25))
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25))
+    model = Column(String(25))
+    manufacturer = Column(String(25))
+    speed = Column(String(25))
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25))
+    model = Column(String(25))
+    manufacturer = Column(String(25))
+    speed = Column(String(25))
 
     def to_dict(self):
         return {}
